8f71272486838dc613dc94af235975478d5b36ca
Adam <spincycle01@yahoo.com>
2019-07-31 21:47:36 -0400

    basic express server set up

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//execute the contents of express and save to app variable
+// for function calls and asynchronicity
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//root route
+app.get('/', (request, response) => {
+  // response.send({ message: '.send from dumb object' });
+  response.json({ message: '.json hi from dumb object' });
+});
+app.get('/happy', (request, response) => {
+  response.send("I'm happy");
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
